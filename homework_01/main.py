41c1a9c28709ebe5a175735d5002b19da1bcc236
Vladimir Perminov <perminov.vl@yandex.ru>
2022-12-26 19:45:41 +0300

    Add homework_01

--- homework_01/main.py
+++ homework_01/main.py
@@ -26,11 +26,13 @@ def filter_numbers(numbers_list, filter_type):
     (выбор производится передачей дополнительного аргумента)
 
     >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
+
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    filter_numbers([2, 1, 3, 5, 4], EVEN)
     if filter_type == ODD:
         return [number for number in numbers_list if number % 2 != 0]
     if filter_type == EVEN:
         return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
