5566da561332ae026f483a691ea81d018583d229
Vladimir Perminov <perminov.vl@yandex.ru>
2022-12-26 21:28:37 +0300

    Add homework_01

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,21 @@
-"""
-создайте класс `Plane`, наследник `Vehicle`
-"""
+from homework_02.base import Vehicle
+from homework_02 import exceptions
+
+
+class Plane(Vehicle):
+    cargo: 0
+    max_cargo: 0
+
+    def __init__(self, max_cargo):
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, num):
+        if (num + self.cargo) <= self.max_cargo:
+            self.cargo += num
+        else:
+            raise exceptions.CargoOverload
+
+    def remove_all_cargo(self):
+        cargo = self.cargo
+        self.cargo = 0
+        return cargo
